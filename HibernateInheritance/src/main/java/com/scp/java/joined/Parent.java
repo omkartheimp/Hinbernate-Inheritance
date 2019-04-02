e01797159691d92e6f903ff7f811235120ddfacd
omkartheimp <omkargavali17@gmail.com>
2019-04-02 20:54:14 +0530

    my hibernate inht commit

---
+++ HibernateInheritance/src/main/java/com/scp/java/joined/Parent.java
@@ -0,0 +1,107 @@
+package com.scp.java.joined;
+
+	import javax.persistence.Column;
+	import javax.persistence.DiscriminatorColumn;
+	import javax.persistence.DiscriminatorValue;
+	import javax.persistence.Entity;
+	import javax.persistence.GeneratedValue;
+	import javax.persistence.GenerationType;
+	import javax.persistence.Id;
+	import javax.persistence.Inheritance;
+	import javax.persistence.InheritanceType;
+	import javax.persistence.Table;
+
+	@Entity
+	@Table(name="parent_child_info3")
+	@Inheritance(strategy=InheritanceType.JOINED)
+	@DiscriminatorValue(value="P")
+	public class Parent {
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	private int id;
+	private String name;
+	private String state ;
+
+	public Parent() {
+		super();
+	}
+
+
+	public Parent(int id, String name, String state) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.state = state;
+	}
+
+
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getState() {
+		return state;
+	}
+	public void setState(String state) {
+		this.state = state;
+	}
+	@Override
+
+
+	public String toString() {
+		return "Parent [id=" + id + ", name=" + name + ", state=" + state + "]";
+	}
+
+	}
+
+	@Entity
+	@DiscriminatorValue(value="C")
+	@Table(name="child3")
+	class Child extends Parent{
+		@Column
+		private String city;
+		private int pin;
+		
+		
+		public Child() {
+			super();
+		}
+		
+		public Child(int id, String name, String state, String city, int pin) {
+			super(id, name, state);
+			this.city = city;
+			this.pin = pin;
+		}
+	 
+		
+		public String getCity() {
+			return city;
+		}
+
+		public void setCity(String city) {
+			this.city = city;
+		}
+
+		public int getPin() {
+			return pin;
+		}
+
+		public void setPin(int pin) {
+			this.pin = pin;
+		}
+
+		@Override
+		public String toString() {
+			return "Child [city=" + city + ", pin=" + pin + "]";
+		}
+		
+	}
+
---
+++ HibernateInheritance/src/main/java/com/scp/java/tableperclass/Test2.java
@@ -0,0 +1,35 @@
+package com.scp.java.tableperclass;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+
+import com.scp.java.tableperclass.Child;
+import com.scp.java.tableperclass.Parent;
+
+public class Test2 {
+	public static void main( String[] args )
+    {
+     
+    	Parent p1=new Parent(10, "Ajya", "Maharashtra");
+    	Child c1=new Child(10, "ajya", "Maharashtra", "Karad", 154154);
+    	
+    	Parent p2=new Parent(15, "pinya", "Maharashtra");
+    	Child c2=new Child(15, "pinya", "Maharashtra", "Sangli", 416416);
+    
+    	SessionFactory sessionFactory=new Configuration().configure().buildSessionFactory();
+     Session session=sessionFactory.openSession();
+     Transaction tr=session.beginTransaction();
+     
+     
+     
+     session.save(p1);
+     session.save(c1);
+     session.save(p2);
+     session.save(c2);
+     
+     session.flush();
+     tr.commit();
+    }
+}
